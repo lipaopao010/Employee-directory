ff9fe500d042ea22c68c5449d1bea1a954111e3b
shuo <shuo.li88@hotmail.com>
2020-07-20 21:07:17 +0800

    add build

---
+++ build/precache-manifest.d8c9e4ec58bf1cc39be00a8399495dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51f7844b091491212bdc4ea70161055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f57fa8b83e6a5134c24",
+    "url": "/static/css/main.55fb6569.chunk.css"
+  },
+  {
+    "revision": "d9e04f90e05f89ba2c8b",
+    "url": "/static/js/2.45e9098b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.45e9098b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f57fa8b83e6a5134c24",
+    "url": "/static/js/main.62baf342.chunk.js"
+  },
+  {
+    "revision": "c4fe6bb0f77d6ca6822c",
+    "url": "/static/js/runtime-main.299b74cd.js"
+  }
+]);
